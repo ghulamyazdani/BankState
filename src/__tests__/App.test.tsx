6642d275672a4b97cefaed0422ac86a409a0b6b4
Ghulam Yazdani <ghulamyazdani12@gmail.com>
2022-01-08 23:17:10 +0530

    tests added

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "../state/index";
+import App from "../App";
+
+test("renders learn react link", async () => {
+  const pet = render(<App />);
+
+  expect(input).toBe(0);
+});
