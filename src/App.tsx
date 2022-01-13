87435d87ceefa79b282b60371926515c727e9703
Ghulam Yazdani <ghulamyazdani12@gmail.com>
2022-01-13 23:47:47 +0530

    Ui changed and added a logo

--- src/App.tsx
+++ src/App.tsx
@@ -15,50 +15,58 @@ function App() {
   );
   const amount = useSelector((state: State) => state.bank);
   return (
-    <div className="h-screen flex justify-center items-center bg-slate-100 ">
-      <div className="w-56 h-56 flex flex-col justify-center items-center bg-white mt-30 p-40 shadow-lg rounded-lg">
-        <h1 className="font-bold text-2xl text-center">Banker app</h1>
-        <h1>{amount}</h1>
-        <form onSubmit={(e) => e.preventDefault()} className="flex flex-col">
-          <input
-            type="number"
-            onChange={(e) => setInput(e.target.value)}
-            value={input}
-          />
-          <div className="w-full flex justify-between mt-3">
-            <button
-              className="bg-blue-500 text-white py-2 px-3 rounded-md mx-auto"
-              onClick={() => {
-                if (input) {
-                  depositMoney(parseInt(input));
+    <div className="h-screen flex flex-col text-center bg-slate-100 ">
+      <h1 className="text-4xl mt-9 font-anton">
+        Bank
+        <i className="fas fa-piggy-bank" />
+        state
+      </h1>
+      <div className="flex justify-center flex-col text-center items-center">
+        <div className="w-56 h-56 flex flex-col justify-center items-center bg-white mt-30 p-40 shadow-lg rounded-lg mt-[12rem]">
+          <h1 className="font-bold text-xl text-center">Current Bal:</h1>
+          <h1 className="text-2xl text">${amount}</h1>
+          <form onSubmit={(e) => e.preventDefault()} className="flex flex-col">
+            <input
+              className="mt-4"
+              type="number"
+              onChange={(e) => setInput(e.target.value)}
+              value={input}
+            />
+            <div className="w-full flex justify-between mt-3">
+              <button
+                className="bg-blue-500 text-white py-2 px-3 rounded-md mx-auto"
+                onClick={() => {
+                  if (input) {
+                    depositMoney(parseInt(input));
+                    setInput("");
+                  }
+                }}
+              >
+                Deposit
+              </button>
+              <button
+                className="bg-blue-500 text-white py-2 px-3 rounded-md mx-auto ml-2"
+                onClick={() => {
+                  if (input) {
+                    withdrawMoney(parseInt(input));
+                    setInput("");
+                  }
+                }}
+              >
+                Withdraw
+              </button>
+              <button
+                className="bg-blue-500 text-white py-2 px-3 rounded-md mx-auto ml-2"
+                onClick={() => {
+                  bankrupt();
                   setInput("");
-                }
-              }}
-            >
-              Deposit
-            </button>
-            <button
-              className="bg-blue-500 text-white py-2 px-3 rounded-md mx-auto ml-2"
-              onClick={() => {
-                if (input) {
-                  withdrawMoney(parseInt(input));
-                  setInput("");
-                }
-              }}
-            >
-              Withdraw
-            </button>
-            <button
-              className="bg-blue-500 text-white py-2 px-3 rounded-md mx-auto ml-2"
-              onClick={() => {
-                bankrupt();
-                setInput("");
-              }}
-            >
-              Bankrupt
-            </button>
-          </div>
-        </form>
+                }}
+              >
+                Bankrupt
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      anton: ["Mochiy Pop P One", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/forms")],
